6d5f9a9208ca5253006a96a45c1667515eb5db80
zarifsajin <147499137+zarifsajin@users.noreply.github.com>
2023-10-17 18:34:25 +0600

    stack task

--- main.cpp
+++ main.cpp
@@ -1,103 +1,61 @@
 #include <iostream>
-
 using namespace std;
-struct student{
-int id, semester_complete,credit_complete;
-float CGPA;
-};
 
-int main (){
-student s[10];
-s[1].id=22-48750-3;
-s[1].semester_complete=4;
-s[1].credit_complete=30;
-s[1].CGPA=3.03;
-cout<<s[1].id<<" "<<endl;
-cout<<s[1].semester_complete<<" "<<endl;
-cout<<s[1].credit_complete<<" "<<endl;
-cout<<s[1].CGPA<<" "<<endl;
+const int size = 50;
+int A[size];
+int top = -1;
 
-s[2].id=22-48650-1;
-s[2].semester_complete=2;
-s[2].credit_complete=20;
-s[2].CGPA=3.33;
-cout<<s[2].id<<" "<<endl;
-cout<<s[2].semester_complete<<" "<<endl;
-cout<<s[2].credit_complete<<" "<<endl;
-cout<<s[2].CGPA<<" "<<endl;
-
-s[3].id=22-21750-3;
-s[3].semester_complete=3;
-s[3].credit_complete=50;
-s[3].CGPA=3.73;
-cout<<s[3].id<<" "<<endl;
-cout<<s[3].semester_complete<<" "<<endl;
-cout<<s[3].credit_complete<<" "<<endl;
-cout<<s[3].CGPA<<" "<<endl;
-
-s[4].id=22-54750-3;
-s[4].semester_complete=7;
-s[4].credit_complete=25;
-s[4].CGPA=3.11;
-cout<<s[4].id<<" "<<endl;
-cout<<s[4].semester_complete<<" "<<endl;
-cout<<s[4].credit_complete<<" "<<endl;
-cout<<s[4].CGPA<<" "<<endl;
-
-s[5].id=22-48950-3;
-s[5].semester_complete=4;
-s[5].credit_complete=38;
-s[5].CGPA=3.07;
-cout<<s[5].id<<" "<<endl;
-cout<<s[5].semester_complete<<" "<<endl;
-cout<<s[5].credit_complete<<" "<<endl;
-cout<<s[5].CGPA<<" "<<endl;
-
-s[6].id=22-48790-3;
-s[6].semester_complete=9;
-s[6].credit_complete=60;
-s[6].CGPA=3.93;
-cout<<s[6].id<<" "<<endl;
-cout<<s[6].semester_complete<<" "<<endl;
-cout<<s[6].credit_complete<<" "<<endl;
-cout<<s[6].CGPA<<" "<<endl;
-
-s[7].id=22-47850-3;
-s[7].semester_complete=8;
-s[7].credit_complete=56;
-s[7].CGPA=3.83;
-cout<<s[7].id<<" "<<endl;
-cout<<s[7].semester_complete<<" "<<endl;
-cout<<s[7].credit_complete<<" "<<endl;
-cout<<s[7].CGPA<<" "<<endl;
-
-s[8].id=22-48767-3;
-s[8].semester_complete=12;
-s[8].credit_complete=90;
-s[8].CGPA=2.03;
-cout<<s[8].id<<" "<<endl;
-cout<<s[8].semester_complete<<" "<<endl;
-cout<<s[8].credit_complete<<" "<<endl;
-cout<<s[8].CGPA<<" "<<endl;
-
-student s9;
-s[9].id=22-46750-3;
-s[9].semester_complete=6;
-s[9].credit_complete=60;
-s[9].CGPA=3.53;
-cout<<s[9].id<<" "<<endl;
-cout<<s[9].semester_complete<<" "<<endl;
-cout<<s[9].credit_complete<<" "<<endl;
-cout<<s[9].CGPA<<" "<<endl;
-
-student s10;
-s[10].id=22-47850-3;
-s[10].semester_complete=9;
-s[10].credit_complete=20;
-s[10].CGPA=1.03;
-cout<<s[10].id<<" "<<endl;
-cout<<s[10].semester_complete<<" "<<endl;
-cout<<s[10].credit_complete<<" "<<endl;
-cout<<s[10].CGPA<<" "<<endl;
+bool isEmpty() {
+    return top == -1;
 }
 
+void push(int value) {
+    if (top == size - 1) {
+        cout << "Stack is full" << endl;
+    } else {
+        top++;
+        A[top] = value;
+    }
+}
+
+void pop() {
+    if (isEmpty()) {
+        cout << "Stack is empty" << endl;
+    } else {
+        top--;
+    }
+}
+
+void showTop() {
+    if (isEmpty()) {
+        cout << "Stack is empty" << endl;
+    } else {
+        cout << "Element at the top is: " << A[top] << endl;
+    }
+}
+
+void displayStack() {
+    if (isEmpty()) {
+        cout << "Stack is empty" << endl;
+    } else {
+        for (int i = 0; i <= top; i++) {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    push(8);
+    push(9);
+    push(10);
+
+    displayStack();
+
+    showTop();
+
+    pop();
+    displayStack();
+
+    return 0;
+}
